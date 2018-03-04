986f58fa6b82e082f97c0690354f040e712191b3
Jacob Erickson <jerickson@csumb.edu>
2018-03-04 05:53:54 +0000

    added profiles and spacing

--- functions.php
+++ functions.php
@@ -43,12 +43,30 @@
         $winnerpoint=0;
         $howmuchpoint=0;
         $winner = '';
+        $names = array('<strong>Alex</strong>', '<strong>Casey</strong>', '<strong>Dylan</strong>', '<strong>Jordan</strong>');
+        $faces = array(
+            '<img src="faces/alpha.jpg" id="profile">',
+            '<img src="faces/beta.jpg" id="profile">',
+            '<img src="faces/gamma.jpg" id="profile">',
+            '<img src="faces/omega.jpg" id="profile">'
+            );
+        $aliases = array();
         
         foreach ($player as $currplay => $currhand)
         {
             $cpt=0;
             
-            echo "<div>" . $currplay;
+            $f_num = rand(0, count($faces) - 1);
+            $n_num = rand(0, count($names) - 1);
+            $aliases[$currplay] = array($names[$n_num], $faces[$f_num]);
+            unset($names[$n_num]);
+            unset($faces[$f_num]);
+            $faces = array_values(array_filter($faces));
+            $names = array_values(array_filter($names));
+            
+            echo "<div>";
+            echo $aliases[$currplay][0];
+            echo $aliases[$currplay][1];
             
             foreach($currhand as $currcard)
             {  
@@ -56,7 +74,7 @@
                 echo $currcard[2];
             }
             
-            echo $cpt;
+            echo "score: " . $cpt;
             echo "</div> <br />";
             
             if($cpt>$winnerpoint && $cpt<=42)
@@ -68,7 +86,10 @@
             $howmuchpoint = $howmuchpoint + $cpt;
         }
         
-        echo "<br/>" . $winner . " Wins! They earned " . ($howmuchpoint - $winnerpoint) . " points!!!";
+        echo "<br/> <div>";
+        echo $aliases[$winner][0];
+        echo $aliases[$winner][1];
+        echo " Wins! They earned " . ($howmuchpoint - $winnerpoint) . " points!!! </div>";
         
     }
 
